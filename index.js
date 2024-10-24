e75b3f0398926cff3327dbd2e694658d400b3004
Garrett <hawkpdx@icloud.com>
2024-10-23 18:27:43 -0700

    /Users/hawkpdx/Development/code/se-prep/phase-0-intro-to-js--2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+//this is only a TEST
+const array = ["Charlie", "Samip", "Ali"];
+
+function writeCards(array) {
+    let messages = [];
+
+for (let i = 0; i < array.length; i++)
+{
+    const name = array[i];
+    messages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+
+}
+    return messages;
+
+}
+console.log(writeCards());
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        --n;
+    }
+}
